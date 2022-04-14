620110371632e86fa4de031110846fd9b53a39a2
quanminh2704 <102666275+quanminh2704@users.noreply.github.com>
2022-04-14 13:28:02 +0700

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,5 +1,6 @@
+
 import pandas as pd
-from setuptools import find_namespace_packages
+import numpy as np
 ##Task 1
 direction = "C:\\Users\\DELL\\Desktop\\FUNIX\\DSP301x_asm2_quandhmFX16121@funix.edu.vn\\Data Files\\"
 file_name_check = True
@@ -7,10 +8,8 @@ while file_name_check:
     file_name = input('Enter file name:\n')
     file_name_record = direction + file_name + '.txt'
     try:
-        with open(file_name_record,'r') as readfile:
-            for x in readfile:
-                file_1 = readfile.readline()
-                print(file_1)
+        with open(file_name_record) as readfile:
+            file_read = readfile.readlines()
     except IOError:
         print('The file cannot be found.')
         file_name_check = False
@@ -18,4 +17,4 @@ while file_name_check:
         print('Successfully opened', file_name + '.txt')
         print('*'*10,'ANALYZING','*'*10)
         print('No errors found')
-        break
+        break    
